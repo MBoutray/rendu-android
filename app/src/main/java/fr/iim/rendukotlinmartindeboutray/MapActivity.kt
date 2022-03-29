3ce6824f5af21016bf1f52bd2f61053452b06981
MBoutray <martindeboutray@hotmail.fr>
2022-03-29 23:55:15 +0200

    Added geocoding

--- app/src/main/java/fr/iim/rendukotlinmartindeboutray/MapActivity.kt
+++ app/src/main/java/fr/iim/rendukotlinmartindeboutray/MapActivity.kt
@@ -1,36 +1,44 @@
 package fr.iim.rendukotlinmartindeboutray
 
+import android.location.Address
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var address: Address
+    private lateinit var returnLocation: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        val location = intent.getStringExtra(EXTRA_LOCATION)
+        // get the value from input of previous activity
+        returnLocation = intent.getStringExtra(EXTRA_LOCATION).toString()
 
-        findViewById<TextView>(R.id.location_activity_text).apply { text = getString(R.string.location_activity_text, location) }
+        // Get the geocoder and return the address corresponding to the location value
+        address = Geocoder(applicationContext, Locale.FRANCE).getFromLocationName(returnLocation, 1)[0]
 
+        // Setup the map
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val location = LatLng(0.0, 0.0)
+        val location = LatLng(address.latitude, address.longitude)
 
         googleMap.addMarker(
             MarkerOptions()
                 .position(location)
-                .title("Marker")
+                .title(returnLocation)
         )
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
     }
