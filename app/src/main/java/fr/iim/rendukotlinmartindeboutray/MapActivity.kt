48d4851fc8eafc040e097aceab7ebeb15e2c3cd1
MBoutray <martindeboutray@hotmail.fr>
2022-03-29 23:26:47 +0200

    Added working map to the MapActivity

--- app/src/main/java/fr/iim/rendukotlinmartindeboutray/MapActivity.kt
+++ app/src/main/java/fr/iim/rendukotlinmartindeboutray/MapActivity.kt
@@ -3,14 +3,35 @@ package fr.iim.rendukotlinmartindeboutray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
         val location = intent.getStringExtra(EXTRA_LOCATION)
 
-        val textView = findViewById<TextView>(R.id.location_activity_text).apply { text = location }
+        findViewById<TextView>(R.id.location_activity_text).apply { text = getString(R.string.location_activity_text, location) }
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val location = LatLng(0.0, 0.0)
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title("Marker")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
     }
 }
